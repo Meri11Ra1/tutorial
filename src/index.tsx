a31cfad3b1c0d0589e33d55a54cbffd7629a57cf
Meri11Ra1 <R.t.00293794@gmail.com>
2022-12-01 09:30:37 +0900

    change directory structure, fix from js to tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,57 +1,21 @@
 import React from "react";
-import './index.css';
+import ReactDOM from "react-dom/client";
+import Game from './screen/App';
 
-const Square = () => {
-      return (
-        <button className="square">
-          {/* TODO */}
-        </button>
-      )
-  }
-  
-  const Board = () => {
-    const renderSquare = (i: number) =>  {
-      return <Square />;
-    }
-  
-      const status = 'Next player: X';
-  
-      return (
-        <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {renderSquare(0)}
-            {renderSquare(1)}
-            {renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {renderSquare(3)}
-            {renderSquare(4)}
-            {renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {renderSquare(6)}
-            {renderSquare(7)}
-            {renderSquare(8)}
-          </div>
-        </div>
-      );
-  }
-  
-  export default function Game(){
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
-        </div>
-      );
-  }
-  
+export default function Main() {
+  return (
+    <div className="index">
+      <Game />
+    </div>
+  )
+}
+
   // ========================================
+
+  const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+  root.render(<Main />);
+
+
+
   
   

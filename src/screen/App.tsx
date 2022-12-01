96e2b773a83b205c8e9dd86a5c7c5f772fb68f09
Meri11Ra1 <R.t.00293794@gmail.com>
2022-12-01 14:06:49 +0900

    initialize value state

--- src/screen/App.tsx
+++ src/screen/App.tsx
@@ -3,53 +3,56 @@ import ReactDOM from "react-dom/client";
 import './App.css';
 
 const Square = (props: any) => {
-      return (
-        <button className="square" onClick={function() { console.log('click'); }}>
-          {props.value}
-        </button>
-      )
+  const [value, setValue] = useState("");
+
+  return (
+    <button className="square" onClick={function() { console.log('click'); }}>
+      {props.value}
+    </button>
+  )
+}
+
+const Board = () => {
+  const renderSquare = (i: number) =>  {
+    return <Square value={i}/>;
   }
-  
-  const Board = () => {
-    const renderSquare = (i: number) =>  {
-      return <Square value={i}/>;
-    }
-  
-      const status = 'Next player: X';
-  
-      return (
-        <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {renderSquare(0)}
-            {renderSquare(1)}
-            {renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {renderSquare(3)}
-            {renderSquare(4)}
-            {renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {renderSquare(6)}
-            {renderSquare(7)}
-            {renderSquare(8)}
-          </div>
+
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
         </div>
-      );
-  }
-  
- export default function Game(){
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
+        <div className="board-row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
         </div>
-      );
-  }
+        <div className="board-row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+      </div>
+    );
+}
+
+export default function Game(){
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+}
+
   
